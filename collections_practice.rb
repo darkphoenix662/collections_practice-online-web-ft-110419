1a002e003c931e02161cbcf71a571be5657ec040
Sean Good <tree662@protonmail.com>
2019-11-13 22:08:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,7 @@ def sort_array_char_count(array)
   array.sort! {|a, b| a.length <=> b.length}
 end
 
-def swap_elements(array, index)
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
