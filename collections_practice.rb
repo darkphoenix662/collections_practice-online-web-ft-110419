618ae10c491ebda56f2514205bac0627dae58e3a
Sean Good <tree662@protonmail.com>
2019-11-13 22:05:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort! {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array, index)
+  array.sort do |name|
+    name[1] <=> name[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+    array
+  end
